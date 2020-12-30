203dfbcf85ed320252a6293e28a817acd93ea08d
Omega <wpb911@gmail.com>
2020-12-30 13:49:53 -0500

    added generate array from prompts and auto generate id numbers to reduce prompts

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const util = require('util');
 const path = require("path");
 const fs = require("fs");
 
@@ -11,30 +12,30 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 //promisify the file object for Async operation
-//const writeFileAsync = util.promisify(fs.writeFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const promptUser = () => {
     return inquirer.prompt([
       {
-            type: 'list',
-            name: 'type',
-            message: 'What type of team member do you wish to enter?',
-            choices: [
-                'Manager',
-                'Engineer',
-                'Intern',                
-            ],
-      },
+        type: 'list',
+        name: 'type',
+        message: 'What type of employee do you wish to enter?',
+        choices: [
+            'Manager',
+            'Engineer',
+            'Intern',                
+        ],
+      },           
       {
         type: 'input',
         name: 'name',
         message: "What is the employee's name?",
       },      
-      {
-        type: 'input',
-        name: 'id',
-        message: "Please provide the employee's id?",
-      },
+      // {
+      //   type: 'input',
+      //   name: 'id',
+      //   message: "Please provide the employee's id?",
+      // },
       {
         type: 'input',
         name: 'email',
@@ -42,29 +43,37 @@ const promptUser = () => {
       },
       {
         type: 'input',
-        name: 'officenumber',
+        name: 'officeNumber',
         message: "Please provide the Manager's office number?",    
         when: (answers) => answers.type === 'Manager'
       },
       {
-          type: 'input',
-          name: 'github',
-          message: "Please provide the Engineer's github username?",
-          when: (answers) => answers.type === 'Engineer'
+        type: 'input',
+        name: 'github',
+        message: "Please provide the Engineer's github username?",
+        when: (answers) => answers.type === 'Engineer'
       },
       {
-          type: 'input',
-          name: 'school',
-          message: "Please provide the Intern's school?",
-          when: (answers) => answers.type === 'Intern'
+        type: 'input',
+        name: 'school',
+        message: "Please provide the Intern's school?",
+        when: (answers) => answers.type === 'Intern'
       },
+      {
+        type: 'confirm',
+        name: 'stop',
+        message: "Is this the last employee you want to enter?",
+        default: false,
+      }, 
       
     
     ]);
 
 };
 
-promptUser();
+
+
+//promptUser();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -88,3 +97,63 @@ promptUser();
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+// Uses async/await and try/catch
+const init = async () => {
+  console.log('hi');
+  try {      
+      
+      //create team array 
+      let team = [];
+      
+      //auto generate the id numbers starting at 1
+      var idnumber = 1;
+
+      //Gather team members until finished
+      while(true) {
+        const answers = await promptUser();
+        
+        //console.log(answers);
+
+        //Add new employees to the team array based on type
+        if(answers.type === "Manager") {
+          team.push(new Manager(answers.name, idnumber, answers.email, answers.officeNumber));
+             
+          console.log(`Id number is ${idnumber}`);   
+          idnumber++;  
+        }
+
+        if(answers.type === "Engineer") {
+          team.push(new Engineer(answers.name, idnumber, answers.email, answers.github)); 
+              
+          console.log(`Id number is ${idnumber}`); 
+          idnumber++;   
+        }
+
+        if(answers.type === "Intern") {
+          team.push(new Intern(answers.name, idnumber, answers.email, answers.school)); 
+             
+          console.log(`Id number is ${idnumber}`); 
+          idnumber++;   
+        }
+
+        console.log(team);
+        //console.log(`Id number is ${idnumber}`);
+        
+        //End the input when no more employees need to be entered
+        if (answers.stop) break;
+        
+
+      }
+
+      // const html = generateREADME(answers);
+      
+      // await writeFileAsync('team.html', html);
+      
+      // console.log('Successfully wrote to team.html');
+      } catch (err) {
+        console.log(err);
+        }
+  };
+  
+  init();
